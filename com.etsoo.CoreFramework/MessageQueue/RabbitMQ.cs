9e7c78db9f9043910eb5d22b08d78e2013188055
garry.xiao <garryxiao@gmail.com>
2021-02-16 05:15:09 +0800

    RabbitMQ

---
+++ com.etsoo.CoreFramework/MessageQueue/RabbitMQ.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.etsoo.CoreFramework.MessageQueue
+{
+    /// <summary>
+    /// RabbitMQ implementation
+    /// https://www.rabbitmq.com/
+    /// </summary>
+    public class RabbitMQ : IMessageQueue
+    {
+        // Factory
+        readonly ConnectionFactory factory;
+
+        // Client name for indication
+        readonly string clientName;
+
+        /// <summary>
+        /// Constructor
+        /// 构造函数
+        /// </summary>
+        /// <param name="initFactory">Initial factory</param>
+        /// <param name="initClientName">Inital client name</param>
+        public RabbitMQ(ConnectionFactory initFactory, string initClientName) => (factory, clientName) = (initFactory, initClientName);
+
+        public void Produce()
+        {
+            using var connection = factory.CreateConnection(clientName);
+
+            connection.CreateModel();
+        }
+    }
+}
