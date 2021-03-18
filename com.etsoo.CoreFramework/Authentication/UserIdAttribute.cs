be20143c1fb3051f093c2831f0b883d4c451487c
Garry Xiao <xz@etsoo.com>
2021-03-18 18:45:47 +1300

    Add UserIdAttribute

---
+++ com.etsoo.CoreFramework/Authentication/UserIdAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.CoreFramework.Authentication
+{
+    /// <summary>
+    /// User id validation attribute
+    /// 用户标识验证属性
+    /// </summary>
+    public class UserIdAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Validity check
+        /// 有效性检查
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <returns>Result</returns>
+        public override bool IsValid(object? value)
+        {
+            if (value is not string valueAsString)
+            {
+                return true;
+            }
+
+            if (valueAsString.Contains('@'))
+            {
+                var email = new EmailAddressAttribute();
+                return email.IsValid(value);
+            }
+            else
+            {
+                var phone = new PhoneAttribute();
+                return phone.IsValid(value);
+            }
+        }
+    }
+}
