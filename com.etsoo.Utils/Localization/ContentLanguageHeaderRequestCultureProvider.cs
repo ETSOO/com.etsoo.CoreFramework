8eaca789e02182c650ebf17477e9bce95dbbde95
Garry Xiao <xz@etsoo.com>
2021-04-13 17:08:39 +1200

    Add ContentLanguageHeaderRequestCultureProvider

---
+++ com.etsoo.Utils/Localization/ContentLanguageHeaderRequestCultureProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace com.etsoo.Utils.Localization
+{
+    /// <summary>
+    /// Determines the culture information for a request via the value of the Content-Language header.
+    /// </summary>
+    public class ContentLanguageHeaderRequestCultureProvider : RequestCultureProvider
+    {
+        /// <inheritdoc />
+        public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            var contentLanguage = httpContext.Request.Headers["Content-Language"].FirstOrDefault();
+
+            if (string.IsNullOrEmpty(contentLanguage))
+            {
+                return NullProviderCultureResult;
+            }
+
+            return Task.FromResult(new ProviderCultureResult(contentLanguage));
+        }
+    }
+}
