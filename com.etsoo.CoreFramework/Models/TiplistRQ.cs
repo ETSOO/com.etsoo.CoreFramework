51e53d56342038e5aa9d3a81947faf7bad336451
Garry Xiao <xz@etsoo.com>
2021-07-13 17:06:52 +1200

    Add TiplistRQ

---
+++ com.etsoo.CoreFramework/Models/TiplistRQ.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+
+namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Tiplist Request data
+    /// 动态列表请求数据
+    /// </summary>
+    public record TiplistRQ<T> where T : struct
+    {
+        /// <summary>
+        /// Current id
+        /// 当前编号
+        /// </summary>
+        public T? Id { get; init; }
+
+        /// <summary>
+        /// Search keyword
+        /// 查询关键词
+        /// </summary>
+        public string? Keyword { get; init; }
+
+        /// <summary>
+        /// Maximum items
+        /// 最大项目数
+        /// </summary>
+        public int? Items { get; init; }
+
+        /// <summary>
+        /// Get parameters
+        /// 获取参数集合
+        /// </summary>
+        /// <returns></returns>
+        public DynamicParameters AsParameters()
+        {
+            var parameters = new DynamicParameters();
+
+            parameters.Add("Id", Id);
+            if (!string.IsNullOrEmpty(Keyword))
+                parameters.Add("Keyword", new DbString { IsAnsi = false, IsFixedLength = false, Length = 50, Value = Keyword });
+            parameters.Add("Items", Items);
+
+            return parameters;
+        }
+    }
+
+    /// <summary>
+    /// Tiplist with int id request data
+    /// 整型编号动态列表请求数据
+    /// </summary>
+    public record TiplistIntRQ : TiplistRQ<int>;
+}
