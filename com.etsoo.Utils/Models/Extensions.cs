4ce3612fb347a07ab0f72024455f314c2edaa427
Garry Xiao <xz@etsoo.com>
2021-09-19 16:46:47 +1200

    Add WhereNotNull to collection

---
+++ com.etsoo.Utils/Models/Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace com.etsoo.Utils.Models
+{
+    /// <summary>
+    /// Extensions
+    /// 扩展
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Get all none null items
+        /// 获取所有非空项目
+        /// </summary>
+        /// <typeparam name="T">Generic item type</typeparam>
+        /// <param name="list">List</param>
+        /// <returns>Result</returns>
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> list)
+        {
+            return list.Where(x => x != null)!;
+        }
+    }
+}
