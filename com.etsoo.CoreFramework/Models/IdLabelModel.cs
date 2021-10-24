fc2a6e02f372d057aabde4f7034f13107bb5f96e
Garry Xiao <xz@etsoo.com>
2021-10-24 22:40:28 +1300

    Add IdLabelModel and IdLabelPrimaryModel

---
+++ com.etsoo.CoreFramework/Models/IdLabelModel.cs
@@ -0,0 +1,16 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Model with id, label and is primary
+    /// 带编号，标签和是否为默认的模型
+    /// </summary>
+    /// <typeparam name="T">Generic id type</typeparam>
+    public record IdLabelPrimaryModel<T> : IdLabelModel<T> where T : struct
+    {
+        /// <summary>
+        /// Is primary
+        /// 是否为默认
+        /// </summary>
+        public bool IsPrimary { get; set; }
+    }
+}
---
+++ com.etsoo.CoreFramework/Models/IdLabelPrimaryModel.cs
@@ -0,0 +1,16 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Model with id and label
+    /// 带编号和标签的模型
+    /// </summary>
+    /// <typeparam name="T">Generic id type</typeparam>
+    public record IdLabelModel<T> : IdModel<T> where T : struct
+    {
+        /// <summary>
+        /// Label
+        /// 标签
+        /// </summary>
+        public string Label { get; set; } = null!;
+    }
+}
