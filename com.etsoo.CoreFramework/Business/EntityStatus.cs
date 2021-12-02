0c657ebbdb30592ff6de9a62b614edce78c9e315
Garry Xiao <xz@etsoo.com>
2021-12-02 20:38:51 +1300

    Add EntityStatus

---
+++ com.etsoo.CoreFramework/Business/EntityStatus.cs
@@ -0,0 +1,39 @@
+﻿namespace com.etsoo.CoreFramework.Business
+{
+    /// <summary>
+    /// Standard entity status enum
+    /// 标准实体状态枚举
+    /// </summary>
+    public enum EntityStatus : byte
+    {
+        /// <summary>
+        /// Normal
+        /// 正常
+        /// </summary>
+        Normal = 0,
+
+        /// <summary>
+        /// Flaged
+        /// 标记的
+        /// </summary>
+        Flaged = 9,
+
+        /// <summary>
+        /// Inactivated
+        /// 已停用
+        /// </summary>
+        Inactivated = 200,
+
+        /// <summary>
+        /// Archived
+        /// 已归档
+        /// </summary>
+        Archived = 254,
+
+        /// <summary>
+        /// Deleted
+        /// 标记删除
+        /// </summary>
+        Deleted = 255
+    }
+}
