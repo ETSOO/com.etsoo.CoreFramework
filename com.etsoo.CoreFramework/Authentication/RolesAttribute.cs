8af28346017f0dddafb5dbd0a8d210f6ebaf2e42
Garry Xiao <xz@etsoo.com>
2022-05-25 12:30:36 +1200

    Add RolesAttribute

---
+++ com.etsoo.CoreFramework/Authentication/RolesAttribute.cs
@@ -0,0 +1,17 @@
+﻿using com.etsoo.WebUtils.Attributes;
+using System.Runtime.Versioning;
+
+namespace com.etsoo.CoreFramework.Authentication
+{
+    /// <summary>
+    /// User roles attribute
+    /// 用户角色属性
+    /// </summary>
+    [RequiresPreviewFeatures]
+    public class RolesAttribute : BaseRolesAttribute<UserRole>
+    {
+        public RolesAttribute(UserRole role) : base(role)
+        {
+        }
+    }
+}
