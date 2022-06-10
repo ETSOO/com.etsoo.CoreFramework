2e75f946eb3564288d5412e11adf03b7f52003f9
Garry Xiao <xz@etsoo.com>
2022-06-10 12:19:36 +1200

    Add JsonSnakeNamingPolicy

---
+++ com.etsoo.Utils/Serialization/JsonSnakeNamingPolicy.cs
@@ -0,0 +1,17 @@
+﻿using com.etsoo.Utils.String;
+using System.Text.Json;
+
+namespace com.etsoo.Utils.Serialization
+{
+    /// <summary>
+    /// Json snake naming policy
+    /// Json 蛇型命名策略
+    /// </summary>
+    public class JsonSnakeNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            return name.ToSnakeCase().ToString();
+        }
+    }
+}
