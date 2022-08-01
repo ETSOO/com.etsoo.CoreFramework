5a71c628fcd04203919ff7d68845e94868dc95ad
Garry Xiao <xz@etsoo.com>
2022-08-01 13:32:54 +1200

    Add ChangePasswordDto

---
+++ com.etsoo.CoreFramework/Models/ChangePasswordDto.cs
@@ -0,0 +1,10 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Change password data
+    /// 修改密码数据
+    /// </summary>
+    /// <param name="OldPassword">Current password</param>
+    /// <param name="Password">New password</param>
+    public record ChangePasswordDto(string OldPassword, string Password);
+}
