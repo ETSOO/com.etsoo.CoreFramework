5f29fcea9fea4b83333a1443890ad4d4d63b3f24
Garry Xiao <xz@etsoo.com>
2022-08-01 12:48:00 +1200

    Add ChangePasswordRQ and SignoutRQ

---
+++ com.etsoo.CoreFramework/Models/ChangePasswordRQ.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Change password request data
+    /// 修改密码请求数据
+    /// </summary>
+    public record ChangePasswordRQ
+    {
+        /// <summary>
+        /// Device id
+        /// 设备编号
+        /// </summary>
+        [Required]
+        [StringLength(512, MinimumLength = 32)]
+        public string DeviceId { get; init; } = null!;
+
+        /// <summary>
+        /// Current password
+        /// 当前密码
+        /// </summary>
+        [StringLength(512, MinimumLength = 64)]
+        public string OldPassword { get; init; } = null!;
+
+        /// <summary>
+        /// New password
+        /// 新密码
+        /// </summary>
+        [StringLength(512, MinimumLength = 64)]
+        public string Password { get; init; } = null!;
+    }
+}
---
+++ com.etsoo.CoreFramework/Models/SignoutRQ.cs
@@ -0,0 +1,9 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Signout request data
+    /// 退出系统请求数据
+    /// </summary>
+    /// <param name="DeviceId">Client device id</param>
+    public record SignoutRQ(string DeviceId);
+}
