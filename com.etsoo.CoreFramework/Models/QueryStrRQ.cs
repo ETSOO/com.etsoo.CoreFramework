01dc6345e38fdb00a1767ff5eb41634b0059a9ed
Garry Xiao <xz@etsoo.com>
2022-08-08 13:58:20 +1200

    Add QueryStrRQ and TiplistStrRQ

---
+++ com.etsoo.CoreFramework/Models/QueryStrRQ.cs
@@ -0,0 +1,15 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Search request data with string id
+    /// 查询请求数据
+    /// </summary>
+    public record QueryStrRQ : QueryRQ<int>
+    {
+        /// <summary>
+        /// String id
+        /// 字符串编号
+        /// </summary>
+        public string? Sid { get; init; }
+    }
+}
---
+++ com.etsoo.CoreFramework/Models/TiplistStrRQ.cs
@@ -0,0 +1,15 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Tiplist Request data with string id
+    /// 动态列表请求数据
+    /// </summary>
+    public record TiplistStrRQ : TiplistRQ<int>
+    {
+        /// <summary>
+        /// String id
+        /// 字符串编号
+        /// </summary>
+        public string? Sid { get; init; }
+    }
+}
