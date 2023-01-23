2ee9b8188348686474d5a87b88905b7f05661929
Garry Xiao <xz@etsoo.com>
2023-01-23 23:04:41 +1300

    Add ExchangeTokenRQ and upgrade dependencies

---
+++ com.etsoo.CoreFramework/Models/ExchangeTokenRQ.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Exchange token request data
+    /// 令牌交换请求数据
+    /// </summary>
+    public record ExchangeTokenRQ
+    {
+        /// <summary>
+        /// Token from core system
+        /// 核心系统的令牌
+        /// </summary>
+        [Required]
+        public string Token { get; init; } = null!;
+    }
+}
