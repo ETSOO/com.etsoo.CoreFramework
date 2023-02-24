17f1f60ee759e8f36d1ea71643389e2515ffa301
Garry Xiao <xz@etsoo.com>
2023-02-24 22:24:01 +1300

    Add DataPrivacy and upgrade dependencies

---
+++ com.etsoo.CoreFramework/Business/DataPrivacy.cs
@@ -0,0 +1,45 @@
+﻿namespace com.etsoo.CoreFramework.Business
+{
+    /// <summary>
+    /// Data privacy
+    /// 数据隐私
+    /// </summary>
+    public enum DataPrivacy : byte
+    {
+        /// <summary>
+        /// Public
+        /// 完全公开
+        /// </summary>
+        Public = 0,
+
+        /// <summary>
+        /// Customer disclosure
+        /// 客户可见
+        /// </summary>
+        Customer = 20,
+
+        /// <summary>
+        /// Internal
+        /// 内部可见
+        /// </summary>
+        Internal = 40,
+
+        /// <summary>
+        /// Dept disclosure
+        /// 部门可见
+        /// </summary>
+        Dept = 60,
+
+        /// <summary>
+        /// Admin disclosure
+        /// 管理员可见
+        /// </summary>
+        Admin = 80,
+
+        /// <summary>
+        /// Private
+        /// 私有
+        /// </summary>
+        Private = 100
+    }
+}
