2fc7a0c5442151e9afda6d0a3ef52c31c3c0482c
Garry Xiao <xz@etsoo.com>
2023-11-06 12:57:14 +1300

    Add SortRQ

---
+++ com.etsoo.CoreFramework/Models/SortRQ.cs
@@ -0,0 +1,21 @@
+﻿namespace com.etsoo.CoreFramework.Models
+{
+    /// <summary>
+    /// Sort with category request data
+    /// 分类数据排序请求数据
+    /// </summary>
+    public record SortRQ
+    {
+        /// <summary>
+        /// Category
+        /// 分类编号
+        /// </summary>
+        public required byte Category { get; init; }
+
+        /// <summary>
+        /// Sort data
+        /// 排序数据
+        /// </summary>
+        public required Dictionary<int, short> Data { get; init; }
+    }
+}
