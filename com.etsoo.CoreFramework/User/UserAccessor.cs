0b0038b0336422498ff90054f4c841b4b03a2ab3
Garry Xiao <xz@etsoo.com>
2024-06-21 18:14:15 +1200

    UserAccessor changes for Minimal API

--- com.etsoo.CoreFramework/User/UserAccessor.cs
+++ com.etsoo.CoreFramework/User/UserAccessor.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using com.etsoo.WebUtils;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 
 namespace com.etsoo.CoreFramework.User
 {
     /// <summary>
-    /// User accessor
-    /// 用户访问器
+    /// User accessor abstract class
+    /// 用户访问器抽象类
     /// </summary>
     /// <remarks>
     /// Constructor
@@ -14,7 +15,7 @@ namespace com.etsoo.CoreFramework.User
     /// </remarks>
     /// <param name="ip">Current IP</param>
     /// <param name="user">User</param>
-    public class UserAccessor<T>(IPAddress ip, T? user) : IUserAccessor<T> where T : IUserCreator<T>
+    public abstract class UserAccessorAbstract<T>(IPAddress ip, T? user) : IUserAccessor<T> where T : IUserCreator<T>
     {
         /// <summary>
         /// Get IP
@@ -43,17 +44,49 @@ namespace com.etsoo.CoreFramework.User
                 return User;
             }
         }
+    }
 
-        /// <summary>
-        /// Constructor
-        /// 构造函数
-        /// </summary>
-        /// <param name="httpContextAccessor">Http context accessor</param>
-        [ActivatorUtilitiesConstructor]
-        public UserAccessor(IHttpContextAccessor httpContextAccessor)
-            : this(httpContextAccessor.HttpContext?.Connection.RemoteIpAddress ?? throw new ArgumentNullException("No IP for user accessor"),
-                  T.Create(httpContextAccessor.HttpContext?.User, httpContextAccessor.HttpContext?.Connection.Id))
-        {
-        }
+    /// <summary>
+    /// User accessor
+    /// 用户访问器
+    /// </summary>
+    /// <remarks>
+    /// Constructor
+    /// 构造函数
+    /// </remarks>
+    /// <param name="httpContextAccessor">Http context accessor</param>
+    [method: ActivatorUtilitiesConstructor]
+    /// <summary>
+    /// User accessor
+    /// 用户访问器
+    /// </summary>
+    public class UserAccessor<T>(IHttpContextAccessor httpContextAccessor)
+        : UserAccessorAbstract<T>(
+            httpContextAccessor.HttpContext?.RemoteIpAddress() ?? throw new Exception("No IP for user accessor"),
+            T.Create(httpContextAccessor.HttpContext?.User, httpContextAccessor.HttpContext?.Connection.Id)
+        ) where T : IUserCreator<T>
+    {
+    }
+
+    /// <summary>
+    /// User accessor for Minimal Api
+    /// 最小接口用户访问器
+    /// </summary>
+    /// <remarks>
+    /// Constructor
+    /// 构造函数
+    /// </remarks>
+    /// <param name="context">HttpContext</param>
+    [method: ActivatorUtilitiesConstructor]
+    /// <summary>
+    /// User accessor
+    /// 用户访问器
+    /// </summary>
+    public class UserAccessorMinimal<T>(HttpContext context)
+        : UserAccessorAbstract<T>(
+            context.RemoteIpAddress() ?? throw new Exception("No IP for user accessor"),
+            T.Create(context.User, context.Connection.Id)
+        ) where T : IUserCreator<T>
+    {
     }
 }
