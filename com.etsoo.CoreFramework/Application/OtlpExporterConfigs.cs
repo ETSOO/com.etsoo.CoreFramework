89fe3634957a68d1eec98163daeef6d22bcf64ab
Garry Xiao <xz@etsoo.com>
2024-08-20 14:46:33 +1200

    Add OtlpExporterConfigs

---
+++ com.etsoo.CoreFramework/Application/OtlpExporterConfigs.cs
@@ -0,0 +1,15 @@
+﻿using OpenTelemetry.Exporter;
+
+namespace com.etsoo.CoreFramework.Application
+{
+    /// <summary>
+    /// OTLP exporter options
+    /// OTLP 导出选项
+    /// </summary>
+    public record OtlpExporterConfigs
+    {
+        public OtlpExportProtocol Protocol { get; set; } = OtlpExportProtocol.HttpProtobuf;
+        public Uri Endpoint { get; set; } = default!;
+        public string? Headers { get; set; }
+    }
+}
