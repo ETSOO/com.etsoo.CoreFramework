318d5e85266ccd91c38644e0bf7a8fc7e8d6e1c3
Garry Xiao <xz@etsoo.com>
2024-09-04 11:44:09 +1200

    Add AuthLoginValidateData to com.etsoo.Web

---
+++ com.etsoo.Web/AuthLoginValidateData.cs
@@ -0,0 +1,29 @@
+﻿using com.etsoo.UserAgentParser;
+
+namespace com.etsoo.Web
+{
+    /// <summary>
+    /// Authorization login validate data
+    /// 授权登录验证数据
+    /// </summary>
+    public record AuthLoginValidateData
+    {
+        /// <summary>
+        /// Device id from client
+        /// 从客户端的设备编号
+        /// </summary>
+        public required string DeviceId { get; init; }
+
+        /// <summary>
+        /// Region
+        /// 区域
+        /// </summary>
+        public required string Region { get; init; }
+
+        /// <summary>
+        /// User agent parser
+        /// 用户代理解析器
+        /// </summary>
+        public required UAParser Parser { get; init; }
+    }
+}
--- com.etsoo.Web/SetupExtensions.cs
+++ com.etsoo.Web/SetupExtensions.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+﻿using com.etsoo.ApiModel.Auth;
+using com.etsoo.CoreFramework.Services;
+using com.etsoo.UserAgentParser;
+using com.etsoo.Utils.Actions;
+using com.etsoo.WebUtils;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
 
 namespace com.etsoo.Web
 {
@@ -8,6 +14,46 @@ namespace com.etsoo.Web
     /// </summary>
     public static class SetupExtensions
     {
+        /// <summary>
+        /// Get OAuth2 user info from client
+        /// 从客户端获取OAuth2用户信息
+        /// </summary>
+        /// <param name="service">Service</param>
+        /// <param name="client">OAuth2 client</param>
+        /// <param name="context">HTPP context</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Result & User info & login validation data</returns>
+        public static async Task<(IActionResult result, AuthUserInfo? userInfo, AuthLoginValidateData? loginData)> GetUserInfoAsync(this IServiceBase service, IAuthClient client, HttpContext context, CancellationToken cancellationToken)
+        {
+            (string DeviceCore, UAParser Parser)? parser = null;
+            string? region = null;
+            string? deviceId = null;
+
+            var (result, userInfo, state) = await client.GetUserInfoAsync(context.Request, (s) =>
+            {
+                // We put the region code like 'CN' at the beginning of the device id
+                region = s[..2];
+
+                // The device id is the rest of the string
+                deviceId = s[2..];
+
+                return service.CheckDevice(context.UserAgent(), deviceId.Replace(" ", "+"), out _, out parser);
+            }, null, cancellationToken);
+
+            AuthLoginValidateData? data = null;
+            if (parser != null && region != null && deviceId != null)
+            {
+                data = new AuthLoginValidateData
+                {
+                    DeviceId = deviceId,
+                    Region = region,
+                    Parser = parser.Value.Parser
+                };
+            }
+
+            return (result, userInfo, data);
+        }
+
         /// <summary>
         /// Setup Cors policy
         /// </summary>
