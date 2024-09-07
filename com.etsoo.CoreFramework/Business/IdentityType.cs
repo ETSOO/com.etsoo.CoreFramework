05428e59a2cb3f7e438f50f10b6631357f109309
Garry Xiao <xz@etsoo.com>
2024-09-07 15:57:56 +1200

    Add IdentityType and IdentityTypeFlags

---
+++ com.etsoo.CoreFramework/Business/IdentityType.cs
@@ -0,0 +1,53 @@
+﻿namespace com.etsoo.CoreFramework.Business
+{
+    /// <summary>
+    /// Identity type
+    /// 标识类型
+    /// </summary>
+    public enum IdentityType : byte
+    {
+        /// <summary>
+        /// User
+        /// 用户
+        /// </summary>
+        User = 1,
+
+        /// <summary>
+        /// Customer
+        /// 客户
+        /// </summary>
+        Customer = 2,
+
+        /// <summary>
+        /// Supplier
+        /// 供应商
+        /// </summary>
+        Supplier = 4,
+    }
+
+    /// <summary>
+    /// Identity type with flags
+    /// 标识类型带标志
+    /// </summary>
+    [Flags]
+    public enum IdentityTypeFlags : byte
+    {
+        /// <summary>
+        /// User
+        /// 用户
+        /// </summary>
+        User = 1,
+
+        /// <summary>
+        /// Customer
+        /// 客户
+        /// </summary>
+        Customer = 2,
+
+        /// <summary>
+        /// Supplier
+        /// 供应商
+        /// </summary>
+        Supplier = 4,
+    }
+}
