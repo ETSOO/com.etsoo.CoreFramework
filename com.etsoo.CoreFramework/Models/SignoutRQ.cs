6d621204dce612bca11ec21c03bdc5bd9d72cd25
Garry Xiao <xz@etsoo.com>
2024-10-11 23:36:53 +1300

    Improve SignoutRQ

--- com.etsoo.CoreFramework/Models/SignoutRQ.cs
+++ com.etsoo.CoreFramework/Models/SignoutRQ.cs
@@ -1,9 +1,25 @@
-﻿namespace com.etsoo.CoreFramework.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.etsoo.CoreFramework.Models
 {
     /// <summary>
     /// Signout request data
     /// 退出系统请求数据
     /// </summary>
-    /// <param name="DeviceId">Client device id</param>
-    public record SignoutRQ(string DeviceId);
+    public record SignoutRQ
+    {
+        /// <summary>
+        /// Device id
+        /// 设备编号
+        /// </summary>
+        [StringLength(512, MinimumLength = 32)]
+        public required string DeviceId { get; init; }
+
+        /// <summary>
+        /// Refresh token
+        /// 刷新令牌
+        /// </summary>
+        [StringLength(512, MinimumLength = 32)]
+        public required string Token { get; init; }
+    }
 }
