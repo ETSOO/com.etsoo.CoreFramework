5344747751131be2f4f64ce63992479a0c08d1c3
Garry Xiao <xz@etsoo.com>
2024-10-16 10:50:37 +1300

    Add Constants

---
+++ com.etsoo.CoreFramework/Application/Constants.cs
@@ -0,0 +1,30 @@
+﻿namespace com.etsoo.CoreFramework.Application
+{
+    /// <summary>
+    /// Constants
+    /// 常量
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Content-Disposition header item name
+        /// </summary>
+        public const string ContentDispositionHeaderName = "Content-Disposition";
+
+        /// <summary>
+        /// Refresh token header item name
+        /// Keep the same with AuthApi.HeaderTokenField@etsoo/appscript
+        /// </summary>
+        public const string RefreshTokenHeaderName = "Etsoo-Refresh-Token";
+
+        /// <summary>
+        /// Current user id parameter name
+        /// </summary>
+        public const string CurrentUserField = "CurrentUser";
+
+        /// <summary>
+        /// Current organization id parameter name
+        /// </summary>
+        public const string CurrentOrgField = "CurrentOrg";
+    }
+}
