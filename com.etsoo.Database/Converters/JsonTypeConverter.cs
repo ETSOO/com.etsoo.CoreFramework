5b8b5110050afe53aba4c98bf1191148a41679a0
Garry Xiao <xz@etsoo.com>
2025-02-18 16:34:13 +1300

    Add JsonTypeConverter

---
+++ com.etsoo.Database/Converters/JsonTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Linq.Expressions;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace com.etsoo.Database.Converters
+{
+    /// <summary>
+    /// Json type converter
+    /// JSON类型转换器
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
+    public class JsonTypeConverter<T> : ValueConverter<T, string?>
+    {
+        public JsonTypeConverter(JsonTypeInfo<T> typeInfo)
+            : base(ToProvider(typeInfo), FromProvider(typeInfo))
+        {
+        }
+
+        private static Expression<Func<T, string?>> ToProvider(JsonTypeInfo<T> typeInfo)
+            => v => JsonSerializer.Serialize(v, typeInfo);
+
+        private static Expression<Func<string?, T>> FromProvider(JsonTypeInfo<T> typeInfo)
+            => v => (string.IsNullOrEmpty(v) ? default : JsonSerializer.Deserialize(v, typeInfo) ?? default)!;
+    }
+}
