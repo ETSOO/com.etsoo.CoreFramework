5c48f2db0e43bf85049abf87bc1b7635f3e480aa
Garry Xiao <xz@etsoo.com>
2025-07-26 00:36:51 +1200

    Add WechatIdAttribute

---
+++ Tests/WebUtils/WechatIdAttributeTests.cs
@@ -0,0 +1,20 @@
+﻿using com.etsoo.WebUtils.Attributes;
+using NUnit.Framework;
+
+namespace Tests.WebUtils
+{
+    [TestFixture]
+    internal class WechatIdAttributeTests
+    {
+        [Test]
+        public void IsValidTests()
+        {
+            var attribute = new WechatIdAttribute();
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(attribute.IsValid("weixin_123"), Is.True);
+                Assert.That(attribute.IsValid("123456"), Is.False);
+            }
+        }
+    }
+}
---
+++ com.etsoo.WebUtils/Attributes/WechatIdAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace com.etsoo.WebUtils.Attributes
+{
+    /// <summary>
+    /// Wechat ID validation attribute
+    /// 微信编号验证属性
+    /// </summary>
+    public partial class WechatIdAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Validity check
+        /// 有效性检查
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <returns>Result</returns>
+        public override bool IsValid(object? value)
+        {
+            if (value == null) return true;
+
+            if (value is not string valueAsString)
+            {
+                return false;
+            }
+
+            // 正则：以字母开头，后面5到19个字符，可以是字母、数字、下划线、减号
+            return MyRegex().IsMatch(valueAsString);
+        }
+
+        /// <summary>
+        /// Override format error message
+        /// 覆盖格式化错误信息
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <returns>Formated string</returns>
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(ErrorMessageString, name);
+        }
+
+        [GeneratedRegex(@"^[a-zA-Z][a-zA-Z0-9_-]{5,19}$")]
+        private static partial Regex MyRegex();
+    }
+}
