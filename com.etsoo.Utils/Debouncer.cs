1c9899eb5ddf1bfd81c610c5ecbdb4af080578cb
Garry Xiao <xz@etsoo.com>
2026-06-21 14:15:36 +0800

    Improve Debouncer

--- com.etsoo.Utils/Debouncer.cs
+++ com.etsoo.Utils/Debouncer.cs
@@ -6,12 +6,14 @@ namespace com.etsoo.Utils
     /// Debouncer
     /// 防抖
     /// </summary>
-    public sealed class Debouncer<TKey> : IDisposable where TKey : notnull
+    public sealed class Debouncer<TKey> : IDisposable, IAsyncDisposable where TKey : notnull
     {
         private readonly CancellationTokenSource _cts = new();
 
         private readonly Func<TKey, CancellationToken, Task> _action;
+        private readonly TimeSpan _delay;
         private readonly int _scanInterval;
+        private readonly bool _overnightCheck;
 
         private readonly ConcurrentDictionary<TKey, DateTime> _pending = new();
 
@@ -20,21 +22,39 @@ namespace com.etsoo.Utils
         /// 构造函数
         /// </summary>
         /// <param name="action">Action to call</param>
+        /// <param name="delay">Delay time for debouncing</param>
         /// <param name="scanInterval">Scan interval in milliseconds, default is 1000</param>
-        public Debouncer(Func<TKey, CancellationToken, Task> action, int scanInterval = 1000)
+        /// <param name="overnightCheck">Whether to check overnight, default is false</param>
+        public Debouncer(Func<TKey, CancellationToken, Task> action,
+            TimeSpan delay,
+            int scanInterval = 1000,
+            bool overnightCheck = true)
         {
             _action = action;
+            _delay = delay;
             _scanInterval = scanInterval;
+            _overnightCheck = overnightCheck;
 
             _ = RunAsync(_cts.Token);
         }
 
-        public void Debounce(TKey key, TimeSpan delay)
+        public void Debounce(TKey key)
         {
-            var timestamp = DateTime.UtcNow.Add(delay);
+            var now = DateTime.UtcNow;
+            var timestamp = now.Add(_delay);
 
             // Add or update the timestamp for the key
             _pending[key] = timestamp;
+
+            // When the timestamp is an overnight time, execute the action immediately
+            if (_overnightCheck && timestamp.Date > now.Date)
+            {
+                var token = _cts.Token;
+                if (_pending.TryRemove(key, out _) && !token.IsCancellationRequested)
+                {
+                    _action(key, token).GetAwaiter().GetResult();
+                }
+            }
         }
 
         private async Task RunAsync(CancellationToken token)
@@ -70,7 +90,7 @@ namespace com.etsoo.Utils
             _pending.Clear();
         }
 
-        public async Task DisposeAsync()
+        public async ValueTask DisposeAsync()
         {
             await _cts.CancelAsync();
             _cts.Dispose();
