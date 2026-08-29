95adec310363cfc8f7b76ab0444e38b7e4fe3518
Garry Xiao <xz@etsoo.com>
2026-08-29 12:10:46 +0800

    Upgrade dependencies

--- com.etsoo.CoreFramework/Application/OtlpExporterConfigs.cs
+++ com.etsoo.CoreFramework/Application/OtlpExporterConfigs.cs
@@ -3,13 +3,30 @@
 namespace com.etsoo.CoreFramework.Application
 {
     /// <summary>
+    /// OTLP exporter service options
+    /// OTLP 导出服务选项
+    /// </summary>
+    public record OtlpExporterService
+    {
+        public OtlpExportProtocol Protocol { get; set; } = OtlpExportProtocol.HttpProtobuf;
+        public Uri Endpoint { get; set; } = default!;
+        public string? Headers { get; set; }
+    }
+
+    /// <summary>
     /// OTLP exporter options
     /// OTLP 导出选项
     /// </summary>
     public record OtlpExporterConfigs
     {
         public OtlpExportProtocol Protocol { get; set; } = OtlpExportProtocol.HttpProtobuf;
-        public Uri Endpoint { get; set; } = default!;
+
         public string? Headers { get; set; }
+
+        public OtlpExporterService Logging { get; set; } = default!;
+
+        public OtlpExporterService? Metrics { get; set; }
+
+        public OtlpExporterService? Tracing { get; set; }
     }
 }
